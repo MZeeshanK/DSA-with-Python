9dc32ac61989534c80dae0ebcb81a63f07969ca6
MZeeshanK <malikzeeshankhurshid@gmail.com>
2023-01-16 18:50:33 +0530

    Created a function to check the balancing of brackets in a string

--- stack.py
+++ stack.py
@@ -22,8 +22,12 @@ class Stack:
 
 def reverse_string(str):
   s = Stack()
+
   for i in str:
      s.push(i)
+     
+  if s.size() <= 1:
+     print('str')
 
   str = ''
 
@@ -33,8 +37,34 @@ def reverse_string(str):
   return str
 
 
+def is_match(ch1, ch2):
+    match = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    }
+    return match[ch1] == ch2
+
+
+def is_balanced(str):
+    s = Stack()
+    for ch in str:
+        if ch=='(' or ch=='{' or ch == '[':
+            s.push(ch)
+        if ch==')' or ch=='}' or ch == ']':
+            if s.size()==0:
+                return False
+            if not is_match(ch,s.pop()):
+                return False
+
+    return s.size()==0
+
 str = 'Hello World'
 print(str)
 
 str = reverse_string(str)
 print(str)
+
+str = '({[Hello World]})'
+print(is_balanced(str))
+
